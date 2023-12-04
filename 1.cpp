d7c9caf7c3bacdba8c137c129c3d96d6fe082196
PinkRammy <victor.vrabete@outlook.com>
2023-12-04 22:03:55 +0200

    Add day 1 input

--- 1.cpp
+++ 1.cpp
@@ -1,38 +1,43 @@
 #include <cstring>
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char **argv) {
 	if (argc != 2) return 0;
 
-	const char* calibrationText = argv[1];
-	size_t calibrationTextLength = strlen(calibrationText);
-	size_t calibrationTextLastIndex = calibrationTextLength - 1;
+	std::ifstream fileStream(argv[1]);
+	if (fileStream.is_open()) {
+		int firstDigit = -1, lastDigit = -1, sum = 0;
+		char calibrationChar; int calibrationCharValue;
 
-	int firstDigit = -1, lastDigit = -1, sum = 0;
-	char calibrationChar;
-	int calibrationCharValue;
-	for (size_t i = 0; i < calibrationTextLength; i++) {
-		calibrationChar = calibrationText[i];
-		if (calibrationChar == '\n' || i == calibrationTextLastIndex) {
+		while(fileStream) {
+			calibrationChar = fileStream.get();
+			if (calibrationChar == '\n') {
+				if (lastDigit < 0) lastDigit = firstDigit;
+				sum += firstDigit * 10 + lastDigit;
+				firstDigit = -1;
+				lastDigit = -1;
+				continue;
+			}
+
+			calibrationCharValue = calibrationChar - '0';
+			if (calibrationCharValue > 9) continue;
+
+			if (firstDigit < 0) {
+				firstDigit = calibrationCharValue;
+				continue;
+			}
+
+			lastDigit = calibrationCharValue;
+		}
+
+		if (firstDigit > 0) {
 			if (lastDigit < 0) lastDigit = firstDigit;
 			sum += firstDigit * 10 + lastDigit;
-
-			firstDigit = -1;
-			lastDigit = -1;
-			continue;
 		}
 
-		calibrationCharValue = calibrationChar - '0';
-		if (calibrationCharValue > 9) continue;
-
-		if (firstDigit < 0) {
-			firstDigit = calibrationCharValue;
-			continue;
-		}
-
-		lastDigit = calibrationCharValue;
+		std::cout << sum << std::endl;
 	}
 
-	std::cout << sum << std::endl;
 	return 0;
 }
